0ce21ead106f6d245daa111a822ba344901bad7d
mohamed elshafiy <blackzetso>
2021-06-08 04:00:58 +0200

    adding section 2 , section3 and loading image and smooth scrolling and fixing all unresponsive divs

--- themes/js/main.js
+++ themes/js/main.js
@@ -36,4 +36,40 @@ $(window).on('resize', function(){
 });
 $(".navbar-toggler").click(function(){
   setTimeout(function(){ test(); });
+}); 
+
+$(window).load(function() {
+  $('#loading').hide();
+}); 
+/* 
+========================================== 
+slow scrolling   
+========================================== 
+*/
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
+//$(window).on('load', function(){
+//    $('#cover').fadeOut(1000);
+//})
